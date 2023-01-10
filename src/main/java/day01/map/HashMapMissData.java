bdf22df79dbab6ddc9d8365cd161cc2fcb739cc0
qingdog <1759765836@qq.com>
2023-01-10 15:55:53 +0800

    hashcode

--- src/main/java/day01/map/HashMapMissData.java
+++ src/main/java/day01/map/HashMapMissData.java
@@ -10,6 +10,9 @@ public class HashMapMissData {
             map.put("a", new Object()); // 97  => 1
         }, "t1");
 
+        // if ((p = tab[i = (n - 1) & hash]) == null)
+        // tab[i] = newNode(hash, key, value, null);
+        // Thread.currentThread().getName().equals("t1")||Thread.currentThread().getName().equals("t2")
         Thread t2 = new Thread(() -> {
             map.put("1", new Object()); // 49 => 1
         }, "t2");
