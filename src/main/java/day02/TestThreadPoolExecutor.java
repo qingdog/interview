7bebea4f4454221f1b4bba73be84f2c1b6ea354f
qingdog <1759765836@qq.com>
2023-01-11 17:11:56 +0800

    lambda

--- src/main/java/day02/TestThreadPoolExecutor.java
+++ src/main/java/day02/TestThreadPoolExecutor.java
@@ -23,14 +23,18 @@ public class TestThreadPoolExecutor {
         ThreadPoolExecutor threadPool = new ThreadPoolExecutor(
                 2,
                 3,
-                0,
+                0, // 救急线程的生存时间，生存时间内没有新任务，此线程资源会释放
                 TimeUnit.MILLISECONDS,
                 queue,
+                // ThreadFactory 是一个接口，它提供了一个 newThread 方法，用来创建新的线程。 lambda 表达式实际上就是实现了这个方法。
+                // 其中 new Thread(r, "myThread" + c.getAndIncrement()) 将runnable r作为参数传入，创建了一个新线程并启动了它。
                 r -> new Thread(r, "myThread" + c.getAndIncrement()),
                 new ThreadPoolExecutor.DiscardOldestPolicy());
         Thread thread = new Thread("myThread" + c.getAndIncrement());
 
         // Thread(Runnable target, String name)
+
+        // 声明具体接口类型 确定lambda表达式所表示的具体抽象方法
         Runnable rr = () -> {};
 
         // 错误的写法，传入的参数Runnable r没有起到作用。实现该接口时应该使用方法里的固定的参数（Runnable r）实现约定的抽象方法（返回Thread对象）
@@ -44,22 +48,20 @@ public class TestThreadPoolExecutor {
         }, "");
 
 
-        ThreadFactory threadFactory2 = r ->  {int  i = 0;return new Thread(r, "");};
-        ThreadFactory threadFactory22 = new ThreadFactory() {
+        // 正确的写法t2和t22。对于有参数的抽象方法可省略小括号（直接定义参数名r箭头指向->方法体），一行代码返回时可省略大括号{}和里面的return。
+        ThreadFactory t2 = r -> {System.out.println(true);return new Thread(r, "");};
+        ThreadFactory t22 = new ThreadFactory() {
             @Override
             public Thread newThread(@NotNull Runnable r) {
                 return new Thread(r ,"");
             }
         };
 
-
         Thread thread1 = new Thread("");
         ThreadFactory threadFactory3 = r -> new Thread("");
 
-
-
         // Thread(Runnable target, String name) {
-        Thread t2 = new Thread(() -> {logger1.debug("before waiting"); },"");
+        Thread t222 = new Thread(() -> {logger1.debug("before waiting"); },"");
 
         showState(queue, threadPool);
         threadPool.submit(new MyTask("1", 3600000));
